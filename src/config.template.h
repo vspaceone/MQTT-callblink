add9883fa71f8d7f29f6d4eee9b6e0ac368bcb3b
Damian Jesionek <Damian.Jes@gmx.de>
2018-12-26 11:21:38 +0100

    Added configuration template

---
+++ src/config.template.h
@@ -0,0 +1,6 @@
+#define wifi_ssid "wifi-ssid"
+#define wifi_password "wifi-password"
+
+#define mqtt_server "mqtt-broker ip"
+#define mqtt_user "mqtt username"
+#define mqtt_password "mqtt password"
